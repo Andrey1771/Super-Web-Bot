9c15e5dbff9a57bd677318bc188492e39e5893b1
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2024-11-10 12:48:48 +0300

    Фикс цвета бэкграунда для game-list-page.tsx

--- tale-gameshop/src/components/game-list-page/game-list-page.tsx
+++ tale-gameshop/src/components/game-list-page/game-list-page.tsx
@@ -56,48 +56,52 @@ class TaleGameshopGameList extends Component<{}, State>{
         const { games, visibleGamesCount } = this.state;
 
         return (
-            <main className="container mx-auto px-4 py-8">
-                <h1 className="text-3xl font-bold text-center mb-4">Game List</h1>
-                <p className="text-center text-gray-600 mb-8">
-                    Browse our extensive collection of computer games, carefully curated to cater to every player's taste.
-                </p>
+            <div className="w-screen bg-gray-100">
+                <main className="container mx-auto px-4 py-8 ">
+                    <h1 className="text-3xl font-bold text-center mb-4">Game List</h1>
+                    <p className="text-center text-gray-600 mb-8">
+                        Browse our extensive collection of computer games, carefully curated to cater to every player's
+                        taste.
+                    </p>
 
-                {Object.keys(this.state.gamesByCategory).map((category) => (
-                    <section key={category}>
-                        <h2 className="text-2xl font-bold mb-4">{category}</h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-                            {this.state.gamesByCategory[category]
-                                .slice(0, visibleGamesCount)
-                                // @ts-ignore
-                                .map((game) => (
-                                    <div key={game.id} className="bg-white p-4 rounded-lg shadow">
-                                        <img
-                                            alt={game.title}
-                                            className="mb-4"
-                                            height="100"
-                                            src={game.image}
-                                            width="100"
-                                        />
-                                        <h2 className="text-xl font-bold mb-2">{game.title}</h2>
-                                        <p className="text-gray-600 mb-4">${game.price}</p>
-                                        <button className="w-full bg-black text-white py-2 rounded hover:text-purple-600">
-                                            Add to Cart
-                                        </button>
-                                    </div>
-                                ))}
-                        </div>
-                    </section>
-                ))}
+                    {Object.keys(this.state.gamesByCategory).map((category) => (
+                        <section key={category}>
+                            <h2 className="text-2xl font-bold mb-4">{category}</h2>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+                                {this.state.gamesByCategory[category]
+                                    .slice(0, visibleGamesCount)
+                                    // @ts-ignore
+                                    .map((game) => (
+                                        <div key={game.id} className="bg-white p-4 rounded-lg shadow">
+                                            <img
+                                                alt={game.title}
+                                                className="mb-4"
+                                                height="100"
+                                                src={game.image}
+                                                width="100"
+                                            />
+                                            <h2 className="text-xl font-bold mb-2">{game.title}</h2>
+                                            <p className="text-gray-600 mb-4">${game.price}</p>
+                                            <button
+                                                className="w-full bg-black text-white py-2 rounded hover:text-purple-600">
+                                                Add to Cart
+                                            </button>
+                                        </div>
+                                    ))}
+                            </div>
+                        </section>
+                    ))}
 
-                {games.length > visibleGamesCount && (
-                    <button
-                        onClick={this.loadMoreGames}
-                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mx-auto block"
-                    >
-                        Load More
-                    </button>
-                )}
-            </main>
+                    {games.length > visibleGamesCount && (
+                        <button
+                            onClick={this.loadMoreGames}
+                            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mx-auto block"
+                        >
+                            Load More
+                        </button>
+                    )}
+                </main>
+            </div>
         );
     }
 }
