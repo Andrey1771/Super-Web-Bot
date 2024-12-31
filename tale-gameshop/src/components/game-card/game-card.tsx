a0239763b27f655a0b0829e223c5e2a519c7b01f
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2024-12-31 11:08:00 +0300

    Переделки game-card

--- tale-gameshop/src/components/game-card/game-card.tsx
+++ tale-gameshop/src/components/game-card/game-card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Game} from '../../models/game';
-import {CartProvider, useCart} from "../../context/cart-context";
+import { Game } from '../../models/game';
+import { useCart } from "../../context/cart-context";
 import { Product } from '../../reducers/cart-reducer';
 import webSettings from '../../webSettings.json';
 
@@ -8,32 +8,47 @@ interface GameCardProps {
     game: Game;
 }
 
-const GameCard: React.FC<GameCardProps> = ({game}) => {
-    const {dispatch} = useCart();
+const GameCard: React.FC<GameCardProps> = ({ game }) => {
+    const { dispatch } = useCart();
 
     const handleAddToCart = () => {
         dispatch({
             type: 'ADD_TO_CART',
-            payload: {gameId: game.id ?? "", name: game.name, price: game.price, quantity: 1, image: game.imagePath} as Product,
+            payload: {
+                gameId: game.id ?? "",
+                name: game.name,
+                price: game.price,
+                quantity: 1,
+                image: game.imagePath
+            } as Product,
         });
     };
 
     return (
-            <div key={game.id} className="bg-white p-4 rounded-lg shadow">
-                <img
-                    alt={game.title}
-                    className="mb-4"
-                    height="100"
-                    src={`${webSettings.apiBaseUrl}/${game.imagePath}`}
-                    width="100"
-                />
-                <h2 className="text-xl font-bold mb-2">{game.title}</h2>
-                <p className="text-gray-600 mb-4">${game.price}</p>
-                <button className="w-full bg-black text-white py-2 rounded hover:text-purple-600"
-                        onClick={handleAddToCart}>
-                    Add to Cart
-                </button>
-            </div>
+        <div key={game.id} className="bg-white p-4 rounded-lg shadow">
+            <img
+                alt={game.title}
+                style={{ height: '100px', width: '100px' }}
+                className="mb-4"
+                height="100px"
+                src={`${webSettings.apiBaseUrl}/${game.imagePath}`}
+                width="100px"
+            />
+            <h2
+                className="text-xl font-bold mb-2 overflow-hidden text-ellipsis whitespace-nowrap"
+                style={{ maxWidth: '200px' }} // Ограничиваем ширину блока
+                title={game.title} // Всплывающая подсказка
+            >
+                {game.title.length > 44 ? `${game.title.slice(0, 44)}...` : game.title}
+            </h2>
+            <p className="text-gray-600 mb-4">${game.price}</p>
+            <button
+                className="w-full bg-black text-white py-2 rounded hover:text-purple-600"
+                onClick={handleAddToCart}
+            >
+                Add to Cart
+            </button>
+        </div>
     );
 };
 
