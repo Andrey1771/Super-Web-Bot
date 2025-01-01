ed1c28fa56fe15ed6b3a176319c7d0d84250b3e3
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2025-01-01 12:58:25 +0300

    Работы над новым game-list-item (2 часть)

--- tale-gameshop/src/components/game-list-item/game-list-item.tsx
+++ tale-gameshop/src/components/game-list-item/game-list-item.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { FixedSizeGrid as Grid } from 'react-window';
 import './game-list-item.css';
 import { Game } from "../../models/game";
@@ -11,12 +11,40 @@ interface GameListItemProps {
 
 const GameListItem: React.FC<GameListItemProps> = ({ filteredGamesByCategory, category }) => {
     const games = filteredGamesByCategory.get(category) || [];
-    const columnCount = 3; // Количество столбцов
-    const rowCount = Math.ceil(games.length / columnCount); // Количество строк
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [containerWidth, setContainerWidth] = useState(0);
+    const [columnCount, setColumnCount] = useState(3);
 
-    const ITEM_WIDTH = 300; // Ширина карточки игры без учета отступов
-    const ITEM_HEIGHT = 200; // Высота карточки игры без учета отступов
-    const GAP = 16; // Отступы между элементами
+    const ITEM_WIDTH = 400; // Ширина карточки игры
+    const ITEM_HEIGHT = 265; // Высота карточки игры
+    const GAP = 24; // Отступы между элементами
+    const MAX_WIDTH = 1504;
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (containerRef.current) {
+                const width = containerRef.current.offsetWidth > MAX_WIDTH ? MAX_WIDTH : containerRef.current.offsetWidth;
+
+                setColumnCount(Math.max(1, Math.floor(width / (ITEM_WIDTH + GAP))));
+                console.log(MAX_WIDTH);
+                console.log(containerRef.current.offsetWidth);
+                console.log(columnCount * (ITEM_WIDTH + GAP) + GAP/*Отступ от скролла*/);
+                setContainerWidth(columnCount * (ITEM_WIDTH + GAP) + GAP/*Отступ от скролла*/);
+            }
+        };
+
+        // Используем ResizeObserver для отслеживания изменений размера контейнера
+        const resizeObserver = new ResizeObserver(() => handleResize());
+        if (containerRef.current) {
+            resizeObserver.observe(containerRef.current);
+        }
+
+        handleResize(); // Вызываем сразу для установки начальных значений
+
+        return () => resizeObserver.disconnect(); // Очищаем наблюдатель
+    }, []);
+
+    const rowCount = Math.ceil(games.length / columnCount);
 
     const Cell = ({ columnIndex, rowIndex, style }: { columnIndex: number; rowIndex: number; style: React.CSSProperties }) => {
         const gameIndex = rowIndex * columnCount + columnIndex;
@@ -29,10 +57,10 @@ const GameListItem: React.FC<GameListItemProps> = ({ filteredGamesByCategory, ca
             <div
                 style={{
                     ...style,
-                    top: Number(style.top) + GAP, // Добавляем отступ сверху
-                    left: Number(style.left) + GAP, // Добавляем отступ слева
-                    width: style.width ? Number(style.width) - GAP : undefined, // Уменьшаем ширину ячейки
-                    height: style.height ? Number(style.height) - GAP : undefined, // Уменьшаем высоту ячейки
+                    top: Number(style.top) + GAP,
+                    left: Number(style.left) + GAP,
+                    width: style.width ? Number(style.width) - GAP : undefined,
+                    height: style.height ? Number(style.height) - GAP : undefined,
                 }}
             >
                 <GameCard key={game.id} game={game} />
@@ -41,20 +69,23 @@ const GameListItem: React.FC<GameListItemProps> = ({ filteredGamesByCategory, ca
     };
 
     return (
-        <section key={category}>
-            <h2 className="text-2xl font-bold mb-4">{category}</h2>
-            <div className="flex justify-between w-full">
-                <Grid
-                    className="mb-8"
-                    columnCount={columnCount}
-                    columnWidth={ITEM_WIDTH + GAP} // Увеличиваем ширину ячейки, чтобы учесть отступы
-                    rowCount={rowCount}
-                    rowHeight={ITEM_HEIGHT + GAP} // Увеличиваем высоту ячейки, чтобы учесть отступы
-                    height={500} // Высота видимой области
-                    width={(ITEM_WIDTH + GAP) * columnCount} // Ширина всей сетки с учетом отступов
-                >
-                    {Cell}
-                </Grid>
+        <section className="flex justify-center flex-col" key={category}>
+            <div className="flex flex-col items-start container">
+                <h2 className="text-2xl font-bold mb-4">{category}</h2>
+                <div ref={containerRef} className="w-full mb-8 flex justify-center" style={{overflow: 'hidden'}}>
+                    {containerWidth > 0 && (
+                        <Grid
+                            columnCount={columnCount}
+                            columnWidth={ITEM_WIDTH + GAP}
+                            rowCount={rowCount}
+                            rowHeight={ITEM_HEIGHT + GAP}
+                            height={500}
+                            width={containerWidth}
+                        >
+                            {Cell}
+                        </Grid>
+                    )}
+                </div>
             </div>
         </section>
     );
