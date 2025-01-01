cff2f7009d0a96fc8f161470ed5568ab5c62cbc1
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2025-01-01 22:31:44 +0300

    Фикс бага с горизонтальным скроллом

--- tale-gameshop/src/components/game-list-page/game-list-page.tsx
+++ tale-gameshop/src/components/game-list-page/game-list-page.tsx
@@ -112,7 +112,7 @@ const TaleGameshopGameList: React.FC = () => {
     );
 
     return (
-        <div className="w-screen bg-gray-100">
+        <div className="bg-gray-100">
             <main className="container mx-auto px-4 py-8">
                 <h1 className="text-3xl font-bold text-center mb-4">Game List</h1>
                 <p className="text-center text-gray-600 mb-8">
