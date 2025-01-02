1aae345391a30ad57121721b9bedc5515a84a47d
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2025-01-02 18:26:40 +0300

    Переделал сервер на https

--- tale-gameshop/server.js
+++ tale-gameshop/server.js
@@ -1,7 +1,12 @@
+import https from 'https';
+import fs from 'fs';
 import express from 'express';
 import path from 'path';
 
 const app = express();
+
+const cert = fs.readFileSync('./public/private.crt');
+const key = fs.readFileSync('./public/private.key');
 
 const buildPath = path.resolve('dist');
 
@@ -13,6 +18,6 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+https.createServer({ key, cert }, app).listen(PORT, () => {
+    console.log(`HTTPS server is running on https://localhost:${PORT}`);
 });
