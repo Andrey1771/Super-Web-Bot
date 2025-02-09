8510c6ec72bc411262e9dffd9c23a8d0fcde3755
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2025-02-09 12:45:59 +0300

    Разделил сервис бота от сайта

---
+++ SuperBot.BotApi/Types/BotConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperBot.WebApi.Types
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; init; } =  default!;
+        public Uri BotWebhookUrl { get; init; } = default!;
+        public string SecretToken { get; init; } = default!;
+    }
+}
