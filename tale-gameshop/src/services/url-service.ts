18683359c12ea4f2c049bb81b22ed114fe09253b
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2026-01-18 06:09:16 +0300

    Use origin-based Keycloak redirect URIs

--- tale-gameshop/src/services/url-service.ts
+++ tale-gameshop/src/services/url-service.ts
@@ -8,12 +8,13 @@ export class UrlService implements IUrlService {
     }
 
     public get keycloak(): KeycloakSettings {
+        const origin = window.location.origin;
         return {
             "url": window.__APP_CONFIG__?.keycloak?.url ?? "http://localhost:8088/",
             "realm": window.__APP_CONFIG__?.keycloak?.realm ?? "TaleShop",
             "clientId": window.__APP_CONFIG__?.keycloak?.clientId ?? "tale-shop-app",
-            "redirectUri": window.__APP_CONFIG__?.keycloak?.redirectUri ?? "http://localhost:3000/callback",
-            "silentCheckSsoRedirectUri": window.__APP_CONFIG__?.keycloak?.silentCheckSsoRedirectUri ?? "http://localhost:3000/silent-check-sso.html",
+            "redirectUri": window.__APP_CONFIG__?.keycloak?.redirectUri ?? origin,
+            "silentCheckSsoRedirectUri": window.__APP_CONFIG__?.keycloak?.silentCheckSsoRedirectUri ?? `${origin}/silent-check-sso.html`,
             "onLoad": window.__APP_CONFIG__?.keycloak?.onLoad ?? "check-sso"
         } as KeycloakSettings;
     }
